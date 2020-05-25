35a1b8c20237608562717d0bb715c421b0c0d45c
Informatica Arelux <informatica@arelux.com>
2020-05-25 09:20:26 +0200

    https://grupoarelux.atlassian.net/browse/ODOOARELUX-772

--- ont_base_picking/models/sale_order.py
+++ ont_base_picking/models/sale_order.py
@@ -13,16 +13,6 @@ class SaleOrder(models.Model):
     shipping_expedition_note = fields.Char(
         string='Nota expedicion',
     )
-    picking_priority = fields.Selection(
-        [
-            ('0', 'No urgente'),
-            ('1', 'Normal'),
-            ('2', 'Urgente'),
-            ('3', 'Muy Urgente'),
-        ],
-        default='1',
-        string='Prioridad albaran',
-    )
 
     def _create_delivery_line(self, carrier, price_unit):
         if price_unit > 0:
@@ -35,10 +25,7 @@ class SaleOrder(models.Model):
         for item in self:
             if item.state == 'sale':
                 for picking_id in item.picking_ids:
-                    picking_id.order_id = item.id
-                    picking_id.confirmation_date_order = item.confirmation_date
                     picking_id.sale_order_note = item.picking_note
                     picking_id.shipping_expedition_note = item.shipping_expedition_note
-                    picking_id.priority = item.picking_priority
         # return
         return return_data
